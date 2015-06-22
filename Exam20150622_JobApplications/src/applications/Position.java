650d451e46175f94d2ff312c8b6fc7be198cfb31
student1566IPU <student1566IPU@6a45ce51-87ed-4bcc-8334-c064a73775c5>
2015-06-22 14:00:02 +0000

    pre-debug commit

--- Exam20150622_JobApplications/src/applications/Position.java
+++ Exam20150622_JobApplications/src/applications/Position.java
@@ -1,17 +1,42 @@
 package applications;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Position {
 
 	
-	public String getName() {return null;}
+	private String name;
+	private ArrayList<Skill> skills;
+	private ArrayList<Applicant> applicants;
+	private String winner = "";
+	public Position(String name) {
+		this.name = name;
+		// TODO Auto-generated constructor stub
+	}
+	public void addSkill(Skill newSkill){
+		this.skills.add(newSkill);
+	}
+	public void addApplicant(Applicant nA){
+		this.applicants.add(nA);
+	}
+
+	public String getName() {return this.name;}
 	
 	public List<String> getApplicants() {
-		return null;
+		// 		return this.positions.stream().sorted((p,q) -> p.getName().compareTo(q.getName())).collect(Collectors.toList());
+		return this.applicants.stream().map(s -> s.getName()).sorted().collect(Collectors.toList());
 	}
 	
 	public String getWinner() {
-		return null; 
+		return this.winner;
+	}
+	public ArrayList<Skill> getSkills() {
+		
+		return this.skills;
+	}
+	public void setWinner(String applicantName) {
+		// TODO Auto-generated method stub
+		this.winner = applicantName;
 	}
 }
--- Exam20150622_JobApplications/src/applications/Skill.java
+++ Exam20150622_JobApplications/src/applications/Skill.java
@@ -1,11 +1,35 @@
 package applications;
 
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 
 public class Skill {
 
 	
-	public String getName() {return null;}
-	public List<Position> getPositions() {return null;}
+	private String name;
+	private Integer level;
+	
+	private ArrayList<Position> positions;
+	public Skill(String name) {
+		this.name = name;
+		// TODO Auto-generated constructor stub
+	}
+	public Skill(String name, Integer level) {
+		this.name = name;
+		this.level = level;
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getLevel()
+	{
+		return this.level;
+	}
+	public String getName() {return this.name;}
+	public void addPosition(Position p){
+		this.positions.add(p);
+	}
+	public List<Position> getPositions() {
+		return this.positions.stream().sorted((p,q) -> p.getName().compareTo(q.getName())).collect(Collectors.toList());
+	}
 }
