a390e10e09bf9a4c4b7d2297d080f1123b53e4e7
student1566IPU <student1566IPU@6a45ce51-87ed-4bcc-8334-c064a73775c5>
2015-06-22 12:54:58 +0000

    completed R1

--- Exam20150622_JobApplications/src/applications/HandleApplications.java
+++ Exam20150622_JobApplications/src/applications/HandleApplications.java
@@ -4,15 +4,43 @@ import java.util.*;
 
 public class HandleApplications {
 
+	private Map<String,Skill> skills = new HashMap<String,Skill>();
+	private Map<String,Position> positions = new HashMap<String,Position>();
 	
 	public void addSkills(String... names) throws ApplicationException {
-		
+		for(String name: names){
+			if(this.skills.containsKey(name)){
+				throw new ApplicationException();
+			} else {
+				this.skills.put(name, new Skill(name));
+			}
+		}
 	}
 	public void addPosition(String name, String... skillNames) throws ApplicationException {
-		
+		if(this.positions.containsKey(name)){
+			throw new ApplicationException();
+		} else {
+			
+			Position newPosition = new Position(name);
+			for(String skillname: skillNames){
+				if(!this.skills.containsKey(name)){
+					throw new ApplicationException();
+				}
+				Skill sk = this.skills.get(skillname);
+				newPosition.addSkill(sk);
+				sk.addPosition(newPosition);
+				this.skills.replace(skillname,sk);
+			}		
+			this.positions.put(name, newPosition);
+		}
 	}
-	public Skill getSkill(String name) {return null;}
-	public Position getPosition(String name) {return null;}
+	
+	public Skill getSkill(String name) {
+		return this.skills.getOrDefault(name, null);
+	}
+	public Position getPosition(String name) {
+		return this.positions.getOrDefault(name,null);
+	}
 	
 	public void addApplicant(String name, String capabilities) throws ApplicationException {
 		
