4d92967129d46533b8006c26454f74a1a7bcbe87
student1566IPU <student1566IPU@6a45ce51-87ed-4bcc-8334-c064a73775c5>
2015-06-22 13:18:35 +0000

    R2 done

--- src/applications/HandleApplications.java
+++ src/applications/HandleApplications.java
@@ -6,6 +6,7 @@ public class HandleApplications {
 
 	private Map<String,Skill> skills = new HashMap<String,Skill>();
 	private Map<String,Position> positions = new HashMap<String,Position>();
+	private Map<String,Applicant> applicants = new HashMap<String,Applicant>();
 	
 	public void addSkills(String... names) throws ApplicationException {
 		for(String name: names){
@@ -43,10 +44,36 @@ public class HandleApplications {
 	}
 	
 	public void addApplicant(String name, String capabilities) throws ApplicationException {
-		
+		if(this.applicants.containsKey(name)){
+			throw new ApplicationException();
+		}
+		ArrayList<Skill> appSkills = new ArrayList<Skill>();
+		String[] skills = capabilities.split(",");
+		for(String skill : skills){
+			String[] arrSkill = skill.split(":");
+			if(arrSkill.length != 2){
+				throw new ApplicationException();
+			}	
+			Integer level;
+			try{
+				level = Integer.valueOf(arrSkill[1]);
+			} catch(Exception e) {
+				throw new ApplicationException();
+			}
+			String skillName = arrSkill[0];
+			if(level > 10 || level < 1){
+				throw new ApplicationException();
+			}
+			appSkills.add(new Skill(skillName,level));
+		}
+		this.applicants.put(name, new Applicant(name,appSkills));
 	}
 	public String getCapabilities(String applicantName) throws ApplicationException {
-		return null;
+		if(this.applicants.containsKey(applicantName)){
+			return this.applicants.get(applicantName).getCapabilities();
+		} else {
+			throw new ApplicationException();
+		}
 	}
 	
 	public void enterApplication(String applicantName, String positionName) throws ApplicationException {
